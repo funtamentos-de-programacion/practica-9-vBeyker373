b8dd881c0d62324fc116ac697437e23b2b01e4b2
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-10 10:55:47 -0600

    Update arreglos1.c

--- arreglos1.c
+++ arreglos1.c
@@ -9,12 +9,24 @@ int main(){
     str2[1] = 'd';
     str2[2] = 'i';
     str2[3] = 'ó';
-    str2[5] = 's';
+    str2[4] = 's';
 
     // TODO: (1) Recorre arr mediante un índice usando un ciclo
+    printf("[ ");
+    for(int i =0; i < 5;i++)
+        printf ("%d, ", arr[i]);
+    printf(" ]\n");
 
     // TODO: (2) Recorre str mediante un índice usando un ciclo
+    printf(" [");
+    for(int m =0; m < 10; m++)
+        printf("%c, ", str[m]);
+    printf(" ]\n");
     
 
     // TODO:(3) Recorre str2 mediante un índice usando un ciclo
+    printf("[ ");
+    for(int n =0; n < 5;n++)
+        printf ("%c, ", str2[n]);
+    printf(" ]\n");
 }
