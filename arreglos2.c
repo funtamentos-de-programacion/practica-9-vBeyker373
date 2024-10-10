5d7961f0c00961550b6356c28cd8cba46b260a64
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-10 10:58:13 -0600

    Update arreglos2.c

--- arreglos2.c
+++ arreglos2.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int arr[]= {1, 2, 3, 4, 5};
     //TODO: (4) Declara un apuntador que apunte al primer elemento del arreglo
+    int arr[]= {1, 2, 3, 4, 5};
+    int *ptr;
+    ptr = arr;
 
+    printf("[ ");
+    for(int i =0; i < 5;i++)
+        printf("%d ", *(ptr + i));
+    printf(" ]\n");
 
     //TODO: (5) Recorre el arreglo utilizando notación de punteros
 
