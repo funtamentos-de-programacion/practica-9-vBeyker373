5c082ecbf41fdcc7740863cabdd18d624905a7ba
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-10 10:59:23 -0600

    Update arreglos4.c

--- arreglos4.c
+++ arreglos4.c
@@ -5,20 +5,24 @@
 //  id: suma
 //  tipo de retorno int
 //  argumento un apuntador a entero y un entero
-
+int suma(int*, int);
 
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
 
     // TODO: (11) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
+    int resultado = suma(arr, 5);
     
     
     // TODO: (12) Imprime el resultado
-    
+    printf("%d", resultado);
     return 0;
 }
 
 
 //TODO: (13) Crea la función suma que realiza la suma de todos los elementos de un arreglo
-
+int suma(int *a, int t){
+    for(int i =0; i < 5; i++)
+        printf(*a);
+}
