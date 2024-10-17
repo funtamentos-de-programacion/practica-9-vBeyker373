a7ba7cec675e8e6b6c2e2728cc4e3bc4a981b059
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-17 10:43:25 -0600

    Update arreglos6.c

--- arreglos6.c
+++ arreglos6.c
@@ -2,12 +2,20 @@
 
 int main() {
     //TODO: (17) Asigna lo valores del 1 al 9 para rellenar la matriz de 3x3
-    int arr[3][3];
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     
     // Acceso usando índices
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             //TODO: (18) Usa i y j para acceder alos elementos de la matriz
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%p\n", &arr[i][j]);
         }
     }
     
