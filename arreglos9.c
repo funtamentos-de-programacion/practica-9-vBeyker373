02ce652df7719bf7bf23348e1fa10c196bc905ae
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-17 10:49:50 -0600

    Update arreglos9.c

--- arreglos9.c
+++ arreglos9.c
@@ -4,19 +4,26 @@ int main() {
     int A[2][2] = {{1, 2}, {3, 4}};
     int B[2][2] = {{5, 6}, {7, 8}};
     int C[2][2] = {0};  // Matriz resultante
+
+    int *ptrA, *ptrB, *ptrC;
+    ptrA= &A[0][0];
+    ptrB = &B[0][0];
+    ptrC = &C[0][0];
     
     // Multiplicación de matrices usando punteros
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             for (int k = 0; k < 2; k++) {
+                // C[i][j] += A[i][k]*B[k][j]
+                *(ptrC+i*2+j) += *(ptrA+i*2+k) * *(ptrB+k)
                 // TODO: Realiza las operaciones solo usando apuntadores
             }
         }
     }
     
     // Mostrar matriz resultante
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++) { //Hacerlo con un solo for
+        for (int j = 0; j < 2; j++) { 
             // TODO: Imprime los elementos de la matriz resultante, usando apuantadores
         }
         
