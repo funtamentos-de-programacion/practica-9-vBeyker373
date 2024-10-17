21077a8e5b41ad99f962f95ffd509a0d33f9f53e
Victor Ventura <ventura.morales.victor@gmail.com>
2024-10-17 10:48:39 -0600

    Update arreglos7.c

--- arreglos7.c
+++ arreglos7.c
@@ -2,14 +2,22 @@
 
 int main() {
     //TODO: (19) Asigna lo valores del 1 al 9 para rellenar la matriz de 3x3
-    int arr[3][3];
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     //TODO: (20) Crea un apuntador que apunte al primer elemento de la matriz
-    
+    int *ptr = &arr[0][0];
+
     // Acceso usando índices
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             //TODO: (21) Usa la lógica de apuntadores para acceder al arreglo
+            printf("%d\t", *(ptr + i*3 +j));
         }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < 9; i++){
+        printf("%d\t", *(ptr + i));
+        ((i+1)%3==0)? printf("\n") : printf("");
     }
     
     return 0;
